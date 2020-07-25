4c01c92ec297e1a221414953f6b543fb644066de
TVitalii <64230122+TVitalii@users.noreply.github.com>
2020-07-25 15:13:32 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,12 @@
 document.querySelector(".hamburger").onclick = function(){
   document.querySelector(".hamburger__plank").classList.toggle("hamburger__plank-active");
-}
+};
 
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
+const menu__list = document.querySelector(".menu__list");
 
 hamburger.addEventListener("click", function () {
   menu.classList.toggle("menu_768");
   menu__list.classList.toggle("menu_768");
-})
+});
